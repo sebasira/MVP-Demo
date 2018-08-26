ed7fed65f95bcb3155935e5f4f695e14cc79d5e0
Sebastian M. Irazabal <sebasira@gmail.com>
2018-08-26 10:18:40 -0300

    Initial commit blank Android Studio project

---
+++ app/src/main/java/ar/com/sebasira/mvp_test/LoginFragment.java
@@ -0,0 +1,23 @@
+package ar.com.sebasira.mvp_test;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class LoginFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_login, container, false);
+
+
+        return rootView;
+    }
+
+
+}
